fbf7e4ae62c67b389ca6aaf00d96d45a1d5549ec
Egor Kharitonov <ega.khar@gmail.com>
2026-01-24 12:32:28 -0500

    feat(home): more questions to faq

--- app/faqData.ts
+++ app/faqData.ts
@@ -12,7 +12,42 @@ export const Questions: Question[] = [
   },
   {
     id: '2',
-    Question: 'I want to attend your events but I am not at USF',
-    Response: 'We welcome all students at our events! Search event you want to visit on Events tab and registrer as a Guest on BullsConnect to visit.'
+    Question: 'What if I am a student not from USF and I want to attend your events?',
+    Response: 'We welcome all students at our events!'
+  },
+  {
+    id: '3',
+    Question: 'What if I am not a computer science, engineering, or computing-related major?',
+    Response: 'No problem at all! We welcome students of all majors. Many fields today work closely with computing "on the edge," whether it\'s healthcare, business, art, science, education, or social sciences. Our goal is to help you understand how computing supports real-world, hands-on work across different industries—no technical background required.'
+  },
+  {
+    id: '4',
+    Question: 'Do I have to have an IEEE-CS membership to go to events?',
+    Response: 'No! Although we encourage buying membership as that increases our potential budget, as a student organization at USF, we welcome everyone to our events!'
+  },
+  {
+    id: '5',
+    Question: 'Can I join at any point in the semester?',
+    Response: 'Yes, you can see all upcoming events at the "Events" tab or on our Bullsconnect.'
+  },
+  {
+    id: '6',
+    Question: 'Is this club beginner-friendly?',
+    Response: 'Yes! While we don\'t focus on traditional "Intro to Python" workshops, all of our workshops are taught step by step and start from the fundamentals. We don\'t assume prior knowledge, and concepts are explained as we go so anyone can follow along - even if you\'re completely new. Moreover, all of our professional development events are applicable to students of a wide range of background and major.'
+  },
+  {
+    id: '7',
+    Question: 'How often do you meet?',
+    Response: 'We usually have 1 hour event every week, excluding first week, test free week and finals weeks. We also often table at Bullsmarket!'
+  },
+  {
+    id: '8',
+    Question: 'What will I get from joining the club?',
+    Response: 'We offer a wide variety of industry-focused skills workshops that aren\'t typically taught in classes. You\'ll also have opportunities to hear directly from guest speakers working in industry, build your professional network, and gain practical insights into real-world careers. Plus, we give out CAT stickers, food, and other fun swag at events!'
+  },
+  {
+    id: '9',
+    Question: 'What happens at a typical event?',
+    Response: 'We usually have a speaker who is either a guest from industry or a tech lead from our executive board. They share their insights on the topic and answer questions. Then, we offer free food and swag. Lastly, participants and executive board members, including the speaker, network and answer any questions from participants. Our events only last around 1 hour.'
   }
 ]
